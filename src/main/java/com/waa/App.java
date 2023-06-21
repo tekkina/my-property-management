cc2207bb48372f2f7e600feef56027108385dff8
Romeo Mugwaneza <romeomugwaneza@gmail.com>
2023-06-20 22:40:41 -0500

    Initialize git project and app congig

--- src/main/java/com/waa/App.java
+++ src/main/java/com/waa/App.java
@@ -1,13 +1,15 @@
 package com.waa;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("===Start===");
+        SpringApplication.run(App.class, args);
+        System.out.println("===End===");
     }
+
 }
